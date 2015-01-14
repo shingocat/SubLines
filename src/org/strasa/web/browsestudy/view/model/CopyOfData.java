17709c667fec0a5f8043111c53f8bb8271192c02
shingocat <shingocat@gmail.com>
2015-01-14 22:41:03 +0800

    redesign data.zul, datagrid.zul under browse study. refactoring data.java the corresponding view model. to implment easy pages raw data. and the "copy of data.zul", "Copy of data grid.zul" and CopyOfData.java" are the original desing. keep these on project for latter viewing or reuse if there are some problem on this update.

--- src/org/strasa/web/browsestudy/view/model/CopyOfData.java
+++ src/org/strasa/web/browsestudy/view/model/CopyOfData.java
@@ -22,8 +22,10 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.Div;
 import org.zkoss.zul.Include;
 
-public class Data {
+public class CopyOfData {
 
+	private int pageSize = 10;
+	private int activePage = 0;
 	private String filePath;
 	private String studyName;
 
@@ -34,31 +36,70 @@ public class Data {
 	private StudyManagerImpl studyMan;
 	private String dataType;
 	private Div divDatagrid;
-	
-	public Data()
-	{
-		//TODO Auto-generated constructor stub
+
+	public CopyOfData() {
+		// TODO Auto-generated constructor stub
 	}
 
-	public List<String> getColumnList()
-	{
-		return columnList;
-	}
-	public List<String[]> getRowData()
-	{
-		return null;
-	}
-	
-	public List<String[]> getDataList()
-	{
-		return dataList;
-	}
-	
-	public Integer getDataListSize()
-	{
+	public int getTotalSize() {
 		return dataList.size();
 	}
-	
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	@NotifyChange("*")
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	@NotifyChange("*")
+	public int getActivePage() {
+		return activePage;
+	}
+
+	@NotifyChange("*")
+	public void setActivePage(int activePage) {
+		System.out.println("pageSize");
+		includeDataGrid();
+		this.activePage = activePage;
+	}
+
+	public List<String> getColumnList() {
+		return columnList;
+	}
+
+	public void setColumnList(List<String> columnList) {
+		this.columnList = columnList;
+		includeDataGrid();
+	}
+
+	public ArrayList<ArrayList<String>> getRowData() {
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		if (dataList.isEmpty())
+			return null;
+		for (int i = activePage * pageSize; i < activePage * pageSize + pageSize && i < dataList.size(); i++) {
+			ArrayList<String> row = new ArrayList<String>();
+			row.addAll(Arrays.asList(dataList.get(i)));
+			result.add(row);
+			row.add(0, "  ");
+			System.out.println(Arrays.toString(dataList.get(i)) + "ROW: " + row.get(0));
+		}
+		return result;
+	}
+
+	public List<String[]> getDataList() {
+		if (true)
+			return dataList;
+		ArrayList<String[]> pageData = new ArrayList<String[]>();
+		for (int i = activePage * pageSize; i < activePage * pageSize + pageSize; i++) {
+			pageData.add(dataList.get(i));
+			System.out.println(Arrays.toString(dataList.get(i)));
+		}
+
+		return pageData;
+	}
 
 	@Init
 	public void init(@ExecutionArgParam("dataType") String dataType, @ExecutionArgParam("studyId") Integer studyId, @ExecutionArgParam("dataset") Integer dataset) {
@@ -74,23 +115,24 @@ public class Data {
 		// System.out.println("StudyId:"+ Integer.toString(studyId));
 		// System.out.println("and dataset:" +Integer.toString(dataset));
 		List<HashMap<String, String>> toreturn = browseStudyManagerImpl.getStudyData(studyId, dataType, dataset);
-//		System.out.println("Size:" + toreturn.size());
+		System.out.println("Size:" + toreturn.size());
 		List<StudyDataColumn> columns = new StudyDataColumnManagerImpl().getStudyDataColumnByStudyId(studyId, dataType, dataset); // rd as raw data, dd as derived data
 																																
 		for (StudyDataColumn d : columns) {
 			columnList.add(d.getColumnheader());
-//			System.out.print(d.getColumnheader() + "\t");
+			System.out.print(d.getColumnheader() + "\t");
 		}
-//		System.out.println("\n ");
+		System.out.println("\n ");
 		for (HashMap<String, String> rec : toreturn) {
 			ArrayList<String> newRow = new ArrayList<String>();
 			for (StudyDataColumn d : columns) {
 				String value = rec.get(d.getColumnheader());
 				newRow.add(value);
-//				System.out.print(value + "\t");
+				System.out.print(value + "\t");
 			}
-//			System.out.println("\n ");
+			System.out.println("\n ");
 			dataList.add(newRow.toArray(new String[newRow.size()]));
+
 		}
 
 		setStudyName(studyMan.getStudyById(studyId).getName());
@@ -106,7 +148,7 @@ public class Data {
 		if (!divDatagrid.getChildren().isEmpty())
 			divDatagrid.getFirstChild().detach();
 		Include incCSVData = new Include();
-		incCSVData.setSrc("/user/browsestudy/datagrid.zul");
+		incCSVData.setSrc("/user/browsestudy/datagrid2.zul");
 		incCSVData.setParent(divDatagrid);
 	}
 
